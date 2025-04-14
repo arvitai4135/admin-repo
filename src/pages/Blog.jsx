c9c36cadf8e567cb78a860e3f9658ededfeb83fe
rahulchaudhary15720 <rahulchaudhary1572002@gmail.com>
2025-04-14 15:03:32 +0530

    Blog issue resolve

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import QuillEditor from '../../../../Blog Editior/src/components/QuillEditor'
 
 const Blog = () => {
   return (
-    <QuillEditor/>
+    <div>Blog</div>
   )
 }
 
